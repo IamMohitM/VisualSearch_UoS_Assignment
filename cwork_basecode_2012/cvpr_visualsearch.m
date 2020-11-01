c2b0e0293608e39198e04600e7e2e65e0325ac7c
Mohit Motwani <mohitmotwani54@gmail.com>
2020-11-01 13:05:12 +0000

    made displaying results optional

--- cwork_basecode_2012/cvpr_visualsearch.m
+++ cwork_basecode_2012/cvpr_visualsearch.m
@@ -16,19 +16,27 @@
 %% (c) John Collomosse 2010  (J.Collomosse@surrey.ac.uk)
 %% Centre for Vision Speech and Signal Processing (CVSSP)
 %% University of Surrey, United Kingdom
+function [precision, recall, average_precision] = cvpr_visualsearch(DESCRIPTOR_SUBFOLDER, nResults, queryClass, displayResults)
 
 close all;
-clear all;
 
 %% Edit the following line to the folder you unzipped the MSRCv2 dataset to
 DATASET_FOLDER = 'MSRC_ObjCategImageDatabase_v2';
-
-%% Folder that holds the results...
 DESCRIPTOR_FOLDER = 'descriptors';
+%% Folder that holds the results...
+if nargin<1    
+    DESCRIPTOR_SUBFOLDER='gridTextureDescriptors_20_20';
+    nResults = 15;
+    displayResults=true;
+elseif nargin < 2
+    nResults = 15;
+    displayResults=true;
+elseif nargin < 4
+    displayResults=true;
+end
 %% and within that folder, another folder to hold the descriptors
 %% we are interested in working with
-DESCRIPTOR_SUBFOLDER='gridTextureDescriptors_20_20';
-
+% DESCRIPTOR_SUBFOLDER='gridTextureDescriptors_20_20';
 
 %% 1) Load all the descriptors into "ALLFEAT"
 %% each row of ALLFEAT is a descriptor (is an image)
@@ -36,12 +44,11 @@ DESCRIPTOR_SUBFOLDER='gridTextureDescriptors_20_20';
 ALLFEAT=[];
 ALLFILES=cell(1,0);
 ctr=1;
-allfiles=dir (fullfile([DATASET_FOLDER,'/Images/*.bmp']));
+allfiles=dir(fullfile([DATASET_FOLDER,'/Images/*.bmp']));
 for filenum=1:length(allfiles)
     fname=allfiles(filenum).name;
     imgfname_full=([DATASET_FOLDER,'/Images/',fname]);
     img=double(imread(imgfname_full))./255;
-    thesefeat=[];
     featfile=[DESCRIPTOR_FOLDER,'/',DESCRIPTOR_SUBFOLDER,'/',fname(1:end-4),'.mat'];%replace .bmp with .mat
     load(featfile,'F');
     ALLFILES{ctr}=imgfname_full;
@@ -50,8 +57,22 @@ for filenum=1:length(allfiles)
 end
 
 %% 2) Pick an image at random to be the query
-NIMG=size(ALLFEAT,1);           % number of images in collection
-queryimg=floor(rand()*NIMG);    % index of a random image
+NIMG=size(ALLFEAT,1); % number of images in collection
+if nargin<3
+    queryimg=floor(rand()*NIMG);    % index of a random image
+else
+    queryClassFiles = {};
+    index = {};
+    classFilesIndex = 0;
+    for i=1:length(allfiles)
+        if(startsWith(allfiles(i).name,strcat(queryClass,'_')))
+            classFilesIndex = classFilesIndex + 1;
+            queryClassFiles{classFilesIndex} = allfiles(i).name;
+            index{classFilesIndex} = i;
+        end
+    end
+    queryimg = index{floor(rand()*classFilesIndex)};
+end
 query_file_path = ALLFILES{queryimg};
 [~, query_file, ~] = fileparts(query_file_path);
 
@@ -69,7 +90,7 @@ dst=sortrows(dst,1);  % sort the results
 %% These may be a little hard to see using imgshow
 %% If you have access, try using imshow(outdisplay) or imagesc(outdisplay)
 
-SHOW=15; % Show top 15 results
+SHOW=nResults; % Show top 15 results
 dst=dst(1:SHOW,:);
 outdisplay=[];
 prediction_files = {};
@@ -78,18 +99,25 @@ for i=1:size(dst,1)
    img=imread(ALLFILES{dst(i,2)});
    [~, name, ~] = fileparts(ALLFILES{dst(i, 2)});
    prediction_files = [prediction_files ; name];
-   img=img(1:2:end,1:2:end,:); % make image a quarter size
-   img=img(1:81,:,:); % crop image to uniform size vertically (some MSVC images are different heights)
-   outdisplay=[outdisplay img];
+   
+   if (displayResults)
+       img=img(1:2:end,1:2:end,:); % make image a quarter size
+       img=img(1:81,:,:); % crop image to uniform size vertically (some MSVC images are different heights)
+       outdisplay=[outdisplay img];
+   end
+   
 end
 
 [precision, recall, average_precision] = evaluate_results(query_file, prediction_files);
-average_precision
-figure;
-imshow(outdisplay);
-axis off;
-figure;
-plot(recall, precision);
-xlabel("Recall");
-ylabel("Precision");
-axis on;
+if (displayResults)
+    figure;
+    imshow(outdisplay);
+    axis off;
+    figure;
+    plot(recall, precision);
+    xlabel("Recall");
+    ylabel("Precision");
+    axis on;
+end
+
+end
