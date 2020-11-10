c23fdd0906854f3a6ae5f0ea02a7136eb224db15
Mohit Motwani <mohitmotwani54@gmail.com>
2020-11-10 15:29:03 +0000

    modified code to calculate ap and map over multiple iterations

--- calculate_mean_average_precision.m
+++ calculate_mean_average_precision.m
@@ -1,15 +1,25 @@
-function mean_average_precision = calculate_mean_average_precision(DESCRIPTOR_SUBFOLDER, nResults)
+function [avgPrecisionPerClass, mean_average_precision, avgPrecisionArray] = calculate_mean_average_precision(DESCRIPTOR_SUBFOLDER, nResults, iterations)
 
 if (nargin<2)
     nResults = 15;
+    iterations = 10;
+elseif (nargin<3)
+    iterations = 10;
 end
 
 totalClasses = 20;
-avgPrecisionArray = zeros(1, totalClasses);
-
+avgPrecisionArray = zeros(1, totalClasses*iterations);
+avgPrecisionPerClass = zeros(1, totalClasses);
+iter = 0;
 for i = 1:totalClasses
-    [~, ~, averagePrecision] = cvpr_visualsearch(DESCRIPTOR_SUBFOLDER, nResults, num2str(i), false);
-    avgPrecisionArray(i) = averagePrecision;
+    disp("Class "+ num2str(i));
+    for j=1:iterations
+        iter = iter + 1;
+        [~, ~, averagePrecision] = cvpr_visualsearch(DESCRIPTOR_SUBFOLDER, '', nResults, num2str(i), false);
+        avgPrecisionArray(iter) = averagePrecision;
+    end
+    avgPrecisionPerClass(i) = mean(avgPrecisionArray((10*(i-1))+1:(10*(i-1))+10));
+    fprintf("Average Precision %2.4f\n", avgPrecisionPerClass(i));
 end
 
 mean_average_precision = mean(avgPrecisionArray);
