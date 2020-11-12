b0ee05bd746536ffad562958028ccf2294c2885a
Mohit Motwani <mohitmotwani54@gmail.com>
2020-11-12 19:28:01 +0000

    support for specificying variance contribution

--- computePCA.m
+++ computePCA.m
@@ -1,5 +1,9 @@
-function computePCA(DESCRIPTOR_SUBFOLDER_PATH, PCA_OUT_FOLDER_PATH, pcaComponents) 
+function computePCA(DESCRIPTOR_SUBFOLDER_PATH, PCA_OUT_FOLDER_PATH, pca_parameter, variance_contribution) 
 
+    if nargin<4
+        variance_contribution = false;
+    end
+    
     DATASET_FOLDER = "MSRC_ObjCategImageDatabase_v2";
     DESCRIPTOR_FOLDER = "descriptors";
     allfiles=dir(fullfile(DATASET_FOLDER+'/Images/*.bmp'));
@@ -14,16 +18,24 @@ function computePCA(DESCRIPTOR_SUBFOLDER_PATH, PCA_OUT_FOLDER_PATH, pcaComponent
         ctr=ctr+1;
     end
     allfeat = ALLFEAT';
+    disp("Building Eigen Model");
     E= Eigen_Build(allfeat);
-    projectionMatrix = Eigen_Deflate(E, 'keepn', pcaComponents);
+    
+    disp("Performing PCA");
+    if variance_contribution
+        projectionMatrix = Eigen_Deflate(E, 'keepf', pca_parameter);
+    else
+        projectionMatrix = Eigen_Deflate(E, 'keepn', pca_parameter);
+    end
+    disp("Projecting Features");
     projected_data = Eigen_Project(allfeat, projectionMatrix)';
     
     %Save the projection Matrix
+    disp("Saving computed features");
     projected_fout = DESCRIPTOR_FOLDER+"/"+ PCA_OUT_FOLDER_PATH+ "/"+"projection_matrix.mat";
     save(projected_fout, "projectionMatrix");
     for i=1:size(projected_data, 1)
         F = projected_data(i, :);
-        
         fname=allfiles(i).name;
         fout=DESCRIPTOR_FOLDER+"/"+PCA_OUT_FOLDER_PATH+"/"+fname(1:end-4)+".mat"; %replace .bmp with .mat
         save(fout,"F"); 
